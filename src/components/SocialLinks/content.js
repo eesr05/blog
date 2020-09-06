2e3e22b6ca95067c75bc59af706d43ff00d16074
Evelinne Rodrigues <48141118+eesr05@users.noreply.github.com>
2020-09-06 20:01:13 -0300

    Adicionando styles

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,16 @@
+const links = [
+  {
+    label: 'Github',
+    url: 'https://github.com/eesr05'
+  },
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com'
+  },
+  {
+    label: 'Instagram',
+    url: 'https://instagram.com'
+  }
+]
+
+export default links
